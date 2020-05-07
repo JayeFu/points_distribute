278a3465dec74f5d4e05bb6c210f7e9ce1360d19
Jiawei Fu <fjw16@mails.tsinghua.edu.cn>
2020-05-07 10:10:20 +0800

    add init and setter

--- src/points_distribute/Optimizer.py
+++ src/points_distribute/Optimizer.py
@@ -1,6 +1,75 @@
 #!/usr/bin/env python
 
+import pandas as pd
+import numpy as np
+
 class SampleOptimizer:
     
-    def __init__(self):
+    def __init__(self, room_dimension=(10.0, 6.0, 3.0)):
+        
+        # room dimension of simula
+        self._room_dimension = room_dimension
+
+        # weights of translation and rotation for traget funciton
+        self._trans_weights = (1.0, 1.0, 1.0)
+        self._rot_weights = (1.0, 1.0, 1.0)
+
+        # range of deviation for sampling
+        self._trans_range = (0.05, 0.05, 0.05)
+        self._rot_range = (0.05, 0.05, 0.05)
+
+        # a list to contain the realtive poses
+        self._relative_poses = list()
+
+
+    def set_trans_weight(self, trans_weights=(1.0, 1.0, 1.0)):
+        """setter function for self._trans_weights
+
+        Keyword Arguments:
+            trans_weights {tuple} -- translation weights for target function (default: {(1.0, 1.0, 1.0)})
+            [0]: mbx
+            [1]: fwx
+            [2]: uav
+        """
+        
+        self._trans_weights = trans_weights
+
+    def set_rot_weights(self, rot_weights=(1.0, 1.0, 1.0)):
+        """setter function for self._rot_weights
+
+        Keyword Arguments:
+            rot_weights {tuple} -- rotation weights for target function (default: {(1.0, 1.0, 1.0)})
+            [0]: mbx
+            [1]: fwx
+            [2]: uav
+        """
+
+        self._rot_weights = rot_weights
+
+    def set_trans_range(self, trans_range=(0.05, 0.05, 0.05)):
+        """setter function for self._trans_range
+
+        Keyword Arguments:
+            trans_range {tuple} -- range of deviation for translation in sampling (default: {(0.05, 0.05, 0.05)})
+            [0]: x
+            [1]: y
+            [2]: z
+        """
+        
+        self._trans_range = trans_range
+
+    def set_rot_range(self, rot_range=(0.05, 0.05, 0.05)):
+        """setter function for self._rot_range
+
+        Keyword Arguments:
+            rot_range {tuple} -- range of deviation for rotation in sampling (default: {(0.05, 0.05, 0.05)})
+            [0]: alpha
+            [1]: beta
+            [2]: gamma
+        """
+
+        self._rot_range = rot_range
+
+    def read_relative_poses(self, file_name="relative_poses.csv"):
+        
         pass
